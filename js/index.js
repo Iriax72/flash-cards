ca3ccd82573cddf865836fbc7d6804382c68cfe8
flexult <touma.leandre.72@gmail.com>
2026-08-29 08:24:42 +0200

    retour en arriere

--- js/index.js
+++ js/index.js
@@ -5,17 +5,13 @@ const addCollecBtn = document.querySelector('#new-collection');
 // Ecrire la page
 for (let i = 0; i < localStorage.length; i++) {
     const collecName = localStorage.key(i);
-    const anchors = []
 
+    // Ajouter l'anchor au HTML
     const a = document.createElement('a');
     a.href = './collection.html';
     a.innerText = collecName;
-    anchors.push(a);
-}
-sorted_anchors = anchors.sort();
-sorted_anchors.forEach(a => {
     main.appendChild(a);
-});
+}
 
 // EventListener
 addCollecBtn.addEventListener('click', () => {
