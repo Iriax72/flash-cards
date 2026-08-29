fee917aff718b9f4fb8bac331a3a93cbe965b2e6
flexult <touma.leandre.72@gmail.com>
2026-08-29 10:28:32 +0200

    ajout du rename de collec

--- js/collection.js
+++ js/collection.js
@@ -4,15 +4,32 @@ const collec = params.has('collection') ? params.get('collection') : null;
 const newCollec = params.has('new') && params.get('new');
 
 // References DOM
+const h1 = document.querySelector('h1');
 const nameForm = document.querySelector('#name-form');
-//const nameFormSubmit = nameForm.querySelector('button[type="submit"]');
+const newNameInput = nameForm.querySelector('input');
 const addCardForm = document.querySelector('#add-card-form');
 //const addCardFormSubmit = addCardForm.querySelector('button[type="submit"]')
+
+// Ecrire la page
+h1.innerText = collec;
 
 // EventListener
 nameForm.addEventListener('submit', (event) => {
     event.preventDefault();
-    alert('Rename de la collec');
+    const newName = newNameInput.value;
+
+    // Empecher de renommer si la collection existe deja
+    if (localStorage.getItem(newName) !== null) {
+        alert('Ce nom est deja utilise')
+        return;
+    }
+
+    // Renommer
+    const data = localStorage.getItem(collec);
+    localStorage.setItem(newName, data);
+    localStorage.removeItem(collec);
+
+    h1.innerText = collec;
 })
 
 addCardForm.addEventListener('submit', (event) => {
