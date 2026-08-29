aa3527ba6c1fcf72a71d262268c67ccecdbb7e58
flexult <touma.leandre.72@gmail.com>
2026-08-29 09:18:41 +0200

    test

--- js/collection.js
+++ js/collection.js
@@ -3,18 +3,19 @@ const params = new URLSearchParams(window.location.search);
 const collec = params.has('collection') ? params.get('collection') : null;
 const newCollec = params.has('new') && params.get('new');
 
-alert('collec: ' + collec + '\n\mnew? :' + newCollec);
 // References DOM
 const nameForm = document.querySelector('#name-form');
+const nameFormSubmit = nameForm.querySelector('button[type="submit"]');
 const addCardForm = document.querySelector('#add-card-form');
+const addCardFormSubmit = addCardForm.querySelector('button[type="submit"]')
 
 // EventListener
-nameForm.addEventListener('submit', (event) => {
+nameFormSubmit.addEventListener('click', (event) => {
     event.preventDefault();
     alert('Rename de la collec');
 })
 
-addCardForm.addEventListener('submit', (event) => {
+addCardFormSubmit.addEventListener('click', (event) => {
     event.preventDefault()
     alert('ajout de la carte');
 });
